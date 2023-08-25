edeee7aba0f796f1094afbbcf64357dffd662bfb
Junior Paula <luke.junnior@icloud.com>
2023-08-25 18:33:08 -0300

    Create a handler to upgrade a connection to websocket

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 // views is a set of jet templates.
@@ -13,9 +14,43 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// Allow all connections by default.
+		return true
+	},
+}
+
 // Home is the handler for the home page route.
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// WsJSONResponse is the response sent to the client when a websocket event occurs.
+type WsJSONResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+// WsEndpoint upgrades connection to websocket.
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected to Endpoint")
+
+	var response WsJSONResponse
+	response.Message = `<em><small>Connected to Server</small></em>`
+
+	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 	}
